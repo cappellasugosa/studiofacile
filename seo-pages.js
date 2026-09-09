3fe40885b6b6f306306504d62d08813cdafde533
cappellasugosa <mattiacosta295@gmail.com>
2026-09-09 15:56:48 +0200

    Delete seo-pages.js

--- seo-pages.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  const links=document.querySelectorAll('[data-tool-link]');
-  links.forEach(a=>a.addEventListener('click',()=>{try{localStorage.setItem('sf-seo-tool',a.dataset.toolLink)}catch(e){}}));
-})();
